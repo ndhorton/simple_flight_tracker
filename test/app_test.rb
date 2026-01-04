12680b9417dbf10cab7d4a7329bcc96a4ae6a35b
Nicholas Horton <nicholashorton7@protonmail.com>
2026-01-04 13:34:50 +0000

    Set up main test file

--- test/app_test.rb
+++ test/app_test.rb
@@ -1,3 +1,22 @@
 # frozen_string_literal: true
 
-require '../app'
+require 'minitest/autorun'
+require 'rack/test'
+
+require_relative '../app'
+
+class SimpleFlightTrackerTest < Minitest::Test
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def admin_session
+    { 'rack.session' => { username: 'admin' } }
+  end
+
+  def session
+    last_request.env['rack.session']
+  end
+end
